7d1f6fb3938471e87cada0b3f6e665a7310fcc8b
immanuellking <emmaebubeegbosi@gmail.com>
2024-02-06 18:17:37 +0100

    set up router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { RouterProvider, createBrowserRouter  } from "react-router-dom";
+import AppLayout from "./pages/AppLayout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />
+  }
+])
+
 function App() {
-  return (
-    <>
-      <div>hello world</div>
-    </>
-  );
+  return <RouterProvider router={router} />
 }
 
 export default App;
