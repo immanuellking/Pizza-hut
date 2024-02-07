4e666da77c1cbbe2e6c28742cf4886a257fa6a88
immanuellking <emmaebubeegbosi@gmail.com>
2024-02-07 14:23:10 +0100

    working on modal, modal visble and displaying informations

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,25 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { formatCurrency } from "../utils/formatters";
+import Modal from "./Modal";
 
 const MenuItem = ({ pizza }) => {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+  const [show, setShow] = useState(false);
+
   return (
-    <div className="rounded-xl border-[1px] overflow-hidden">
-      <div className="h-52 w-full overflow-hidden">
-        <img src={imageUrl} alt="pizza" className="w-full h-full scale-100 hover:scale-110 duration-200 ease-in" />
-      </div>
-      <div className="px-3 py-4 space-y-3">
-        <div className="flex justify-between">
-          <h3 className="text-base">{name}</h3>
-          <p className="font-semibold">{formatCurrency(unitPrice)}</p>
+    <>
+      <div
+        className="rounded-xl border-[1px] overflow-hidden cursor-pointer group"
+        onClick={() => setShow(true)}
+      >
+        <div className="h-52 w-full overflow-hidden">
+          <img
+            src={imageUrl}
+            alt="pizza"
+            className="w-full h-full scale-100 group-hover:scale-110 duration-200 ease-in"
+          />
         </div>
-        <div className="flex flex-wrap gap-x-1">
-          {ingredients.map((ingredient, index) => (
-            <p className="text-sm text-[#a4a7ae]" key={index}>{ingredient}</p>
-          ))}
+        <div className="px-3 py-4 space-y-3">
+          <div className="flex justify-between">
+            <h3 className="text-base">{name}</h3>
+            <p className="font-semibold">{formatCurrency(unitPrice)}</p>
+          </div>
+          <div className="flex flex-wrap gap-x-1">
+            {ingredients.map((ingredient, index) => (
+              <p className="text-sm text-[#a4a7ae]" key={index}>
+                {ingredient}
+              </p>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+
+      <Modal show={show} setShow={setShow} pizza={pizza} />
+    </>
   );
 };
 
---
+++ src/components/Modal.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect } from "react";
+import { formatCurrency } from "../utils/formatters";
+
+const Modal = ({ show, pizza, setShow }) => {
+  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      document.body.style.overflow = "visible";
+    };
+  }, []);
+  return (
+    <>
+      {show && (
+        <div
+          className="fixed inset-0 bg-[rgba(0,0,0,0.3)] flex justify-center items-center overscroll-none overflow-hidden"
+          style={{ zIndex: 2000 }}
+        >
+          <main className="min-w-[30rem] bg-white px-5 py-5 relative">
+            <img src={imageUrl} alt="pizza" className="w-full h-72" />
+            <div className="px-3 py-4 space-y-3">
+              <div className="flex justify-between">
+                <h3 className="text-base font-bold">{name}</h3>
+                <p className="font-semibold">{formatCurrency(unitPrice)}</p>
+              </div>
+              <div className="flex flex-col">
+                <h2 className="">Ingredients :</h2>
+                <div className="flex flex-wrap gap-x-1">
+                  {ingredients.map((ingredient, index) => (
+                    <p className="text-sm text-[#a4a7ae]" key={index}>
+                      {ingredient}
+                    </p>
+                  ))}
+                </div>
+              </div>
+            </div>
+
+            <div
+              className="absolute right-7 top-7 bg-[#F4F5F8] h-11 w-11 rounded-full flex justify-center items-center cursor-pointer"
+              onClick={() => setShow(false)}
+            >
+              <span className="text-4xl">&times;</span>
+            </div>
+          </main>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Modal;
