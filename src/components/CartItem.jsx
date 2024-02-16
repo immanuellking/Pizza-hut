6c2b2ecf51294c5579ffaf98ec5b5dd6eab80037
immanuellking <emmaebubeegbosi@gmail.com>
2024-02-16 21:35:19 +0100

    changed id to pizzaId resolved svg error in console

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -7,7 +7,7 @@ import { cartActionCreators } from "../redux/cart";
 import { useDispatch } from "react-redux";
 
 const CartItem = ({ item, calcTotal }) => {
-  const { id, name, quantity, totalPrice, imageUrl, ingredients } = item;
+  const { pizzaId, name, quantity, totalPrice, imageUrl, ingredients } = item;
 
   const dispatch = useDispatch();
 
@@ -15,17 +15,17 @@ const CartItem = ({ item, calcTotal }) => {
     bindActionCreators(cartActionCreators, dispatch);
 
   const increaseAmount = () => {
-    increaseItemQuantity(id);
+    increaseItemQuantity(pizzaId);
     calcTotal();
   };
 
   const decreaseAmount = () => {
-    decreaseItemQuantity(id);
+    decreaseItemQuantity(pizzaId);
     calcTotal();
   };
 
   const deleteItem = () => {
-    deleteItemFromCart(id);
+    deleteItemFromCart(pizzaId);
     calcTotal();
   };
 
@@ -71,37 +71,37 @@ const CartItem = ({ item, calcTotal }) => {
                   <path
                     d="M14 3.98665C11.78 3.76665 9.54667 3.65332 7.32 3.65332C6 3.65332 4.68 3.71999 3.36 3.85332L2 3.98665"
                     stroke="#FFFFFF"
-                    stroke-width="1.2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeWidth="1.2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                   ></path>
                   <path
                     d="M5.66669 3.31337L5.81335 2.44004C5.92002 1.80671 6.00002 1.33337 7.12669 1.33337H8.87335C10 1.33337 10.0867 1.83337 10.1867 2.44671L10.3334 3.31337"
                     stroke="#FFFFFF"
-                    stroke-width="1.2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeWidth="1.2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                   ></path>
                   <path
                     d="M12.5667 6.09338L12.1334 12.8067C12.06 13.8534 12 14.6667 10.14 14.6667H5.86002C4.00002 14.6667 3.94002 13.8534 3.86668 12.8067L3.43335 6.09338"
                     stroke="#FFFFFF"
-                    stroke-width="1.2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeWidth="1.2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                   ></path>
                   <path
                     d="M6.88666 11H9.10666"
                     stroke="#FFFFFF"
-                    stroke-width="1.2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeWidth="1.2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                   ></path>
                   <path
                     d="M6.33331 8.33337H9.66665"
                     stroke="#FFFFFF"
-                    stroke-width="1.2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeWidth="1.2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                   ></path>
                 </svg>
               </Button>
