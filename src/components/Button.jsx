08b303b7596516b59213524d1104237d1ef95751
immanuellking <emmaebubeegbosi@gmail.com>
2024-02-16 09:22:24 +0100

    made style changes to the primary button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,7 +6,7 @@ const Button = ({ children, type, onClick, to }) => {
     " text-sm rounded-full font-semibold capitalize tracking-wide transition-colors duration-300 ";
   const styles = {
     round: `${base}bg-gray-400 text-stone-800 hover:bg-gray-300 focus:outline-none focus:ring focus:ring-gray-400 focus:ring-offset-2 disabled:cursor-not-allowed p-1.5 sm:px-2 sm:py-2`,
-    primary: `${base} bg-black text-white p-2 sm:px-12 sm:py-3`,
+    primary: `${base} bg-black text-white p-2 sm:px-12 sm:py-2.5`,
     secondary: `${base} bg-black text-white px-20 py-4 text-lg`,
   };
 
