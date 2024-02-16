a90bf79bad48e566749aa94a889feda014708cbb
immanuellking <emmaebubeegbosi@gmail.com>
2024-02-16 10:48:12 +0100

    added phone number validation regex made form responsive and alos the action function for the react router

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,6 +1,17 @@
 import React, { useState } from "react";
 import BackNav from "../components/BackNav";
 import Button from "../components/Button";
+import { Form } from "react-router-dom";
+
+const isValidPhone = (str) =>
+  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
+    str
+  );
+
+export async function action({ request }) {
+  const awaitedData = await request.formData();
+  console.log(awaitedData);
+}
 
 const Checkout = () => {
   const [priority, setPriority] = useState(false);
@@ -8,107 +19,109 @@ const Checkout = () => {
     <div className="w-[95%] sm:w-[90%] lg:w-[70%] mx-auto my-5 sm:my-10">
       <BackNav title="Checkout" />
 
-      <main className="w-[70%] mx-auto">
-        <h1 className="text-xl font-bold mb-5">Delivery Information</h1>
-        <form action="" className="space-y-5">
-          <div className="flex flex-col gap-y-2">
-            <label>Full Name</label>
-            <input
-              type="text"
-              placeholder="John James"
-              aria-label="Full Name"
-              name="full_name"
-              className="border-[2px] border-[#EEEFF2] rounded-lg  px-2 py-2.5 focus:outline-[#aca9a9] valid:border-green-500"
-              required
-            />
-          </div>
-
-          <div className="flex flex-col gap-y-2">
-            <label>Email Address</label>
-            <input
-              type="email"
-              placeholder="example@gmail.com"
-              aria-label="Email Address"
-              className="border-[2px] border-[#EEEFF2] rounded-lg  px-2 py-2.5 focus:outline-[#aca9a9] valid:border-green-500"
-              name="email_address"
-              required
-            />
-          </div>
-
-          <div className="flex flex-col gap-y-2">
-            <label>Phone Number</label>
-            <div className="w-full flex gap-x-4">
-              <div className="flex items-center border-[1px] px-4 space-x-3 rounded-lg">
-                <img
-                  alt="Ngn"
-                  src="nigeria.svg"
-                  width="20"
-                  height="20"
-                  decoding="async"
-                  data-nimg="1"
-                  loading="lazy"
-                  style={{ color: "transparent" }}
-                />
-                <span>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    viewBox="0 0 16 16"
-                    fill="none"
-                  >
-                    <path
-                      d="M13.28 5.96667L8.9333 10.3133C8.41997 10.8267 7.57997 10.8267 7.06664 10.3133L2.71997 5.96667"
-                      stroke="#7E8494"
-                      stroke-width="1.5"
-                      stroke-miterlimit="10"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    ></path>
-                  </svg>
-                </span>
-              </div>
+      <main className="sm:w-[80%] lg:w-[70%] mx-auto">
+        <h1 className="text-xl font-bold mb-5 text-center sm:text-left">Delivery Information</h1>
+        <Form>
+          <div className="space-y-6">
+            <div className="flex flex-col gap-y-2">
+              <label>Full Name</label>
               <input
-                type="tel"
-                placeholder="912 345 6789"
-                aria-label="Email Address"
-                className="border-[2px] border-[#EEEFF2] rounded-lg  px-2 py-2.5 focus:outline-[#aca9a9] valid:border-green-500 w-full"
-                name="phone_no"
+                type="text"
+                placeholder="John James"
+                aria-label="Full Name"
+                name="full_name"
+                className="border-[2px] border-[#EEEFF2] rounded-lg  px-2 py-2.5 focus:outline-[#aca9a9] valid:border-green-500"
                 required
               />
             </div>
+
+            <div className="flex flex-col gap-y-2">
+              <label>Email Address</label>
+              <input
+                type="email"
+                placeholder="example@gmail.com"
+                aria-label="Email Address"
+                className="border-[2px] border-[#EEEFF2] rounded-lg  px-2 py-2.5 focus:outline-[#aca9a9] valid:border-green-500"
+                name="email_address"
+                required
+              />
+            </div>
+
+            <div className="flex flex-col gap-y-2">
+              <label>Phone Number</label>
+              <div className="w-full flex gap-x-4">
+                <div className="flex items-center border-[1px] px-4 space-x-3 rounded-lg">
+                  <img
+                    alt="Ngn"
+                    src="nigeria.svg"
+                    width="20"
+                    height="20"
+                    decoding="async"
+                    data-nimg="1"
+                    loading="lazy"
+                    style={{ color: "transparent" }}
+                  />
+                  <span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      viewBox="0 0 16 16"
+                      fill="none"
+                    >
+                      <path
+                        d="M13.28 5.96667L8.9333 10.3133C8.41997 10.8267 7.57997 10.8267 7.06664 10.3133L2.71997 5.96667"
+                        stroke="#7E8494"
+                        stroke-width="1.5"
+                        stroke-miterlimit="10"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      ></path>
+                    </svg>
+                  </span>
+                </div>
+                <input
+                  type="tel"
+                  placeholder="912 345 6789"
+                  aria-label="Email Address"
+                  className="border-[2px] border-[#EEEFF2] rounded-lg  px-2 py-2.5 focus:outline-[#aca9a9] valid:border-green-500 w-full"
+                  name="phone_no"
+                  required
+                />
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-y-2">
+              <label>Delivery Address</label>
+              <input
+                type="text"
+                placeholder="23, Landmark Street, Lekki, Lagos"
+                aria-label="Delivery Address"
+                className="border-[2px] border-[#EEEFF2] rounded-lg focus:outline-[#aca9a9]  px-2 py-2.5 valid:border-green-500"
+                name="delivery_address"
+                required
+              />
+            </div>
+
+            <div className="flex items-center gap-x-3 mb-20">
+              <input
+                type="checkbox"
+                aria-label="priority"
+                value={priority}
+                onChange={(e) => setPriority(e.target.checked)}
+                name="priority"
+                className="h-6 w-6 cursor-pointer checked:accent-black"
+              />
+              <label className="text-lg font-semibold">
+                Give your order priority ?
+              </label>
+            </div>
           </div>
 
-          <div className="flex flex-col gap-y-2">
-            <label>Delivery Address</label>
-            <input
-              type="text"
-              placeholder="23, Landmark Street, Lekki, Lagos"
-              aria-label="Delivery Address"
-              className="border-[2px] border-[#EEEFF2] rounded-lg focus:outline-[#aca9a9]  px-2 py-2.5 valid:border-green-500"
-              name="delivery_address"
-              required
-            />
+          <div className="flex justify-center mt-10">
+            <Button type="secondary">Checkout</Button>
           </div>
-
-          <div className="flex items-center gap-x-3">
-            <input
-              type="checkbox"
-              aria-label="priority"
-              value={priority}
-              onChange={(e) => setPriority(e.target.checked)}
-              name="priority"
-              className="h-6 w-6 cursor-pointer checked:accent-black"
-            />
-            <label className="text-lg font-semibold">Give your order priority ?</label>
-          </div>
-
-          <div className="flex justify-center mt-5">
-            <Button type="secondary" onClick>
-              Checkout
-            </Button>
-          </div>
-        </form>
+        </Form>
       </main>
     </div>
   );
