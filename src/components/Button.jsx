517ce16c2d8f366992bb88a0da99dc9f617a28b2
immanuellking <emmaebubeegbosi@gmail.com>
2024-02-21 18:50:52 +0100

    added the disabled and disabled style and prop to the Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Button = ({ children, type, onClick, to }) => {
+const Button = ({ children, type, onClick, to, disabled }) => {
   const base =
     " text-sm rounded-full font-semibold capitalize tracking-wide transition-colors duration-300 ";
   const styles = {
     round: `${base}bg-gray-400 text-stone-800 hover:bg-gray-300 focus:outline-none focus:ring focus:ring-gray-400 focus:ring-offset-2 disabled:cursor-not-allowed p-1.5 sm:px-2 sm:py-2`,
     primary: `${base} bg-black text-white p-2 sm:px-12 sm:py-2.5`,
     secondary: `${base} bg-black text-white px-20 py-4 text-lg`,
+    disabled: `${base} bg-[rgba(0,0,0,0.7)] text-white px-20 py-4 text-lg cursor-not-allowed`,
   };
 
   if (to) {
@@ -26,11 +27,10 @@ const Button = ({ children, type, onClick, to }) => {
   }
 
   return (
-    <button className={styles[type]}>
+    <button disabled={disabled} className={styles[type]}>
       {children}
     </button>
   );
-
 };
 
 export default Button;
