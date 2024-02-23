b4c9489d1ec93fe7747b66da840dc114696cfe89
immanuellking <emmaebubeegbosi@gmail.com>
2024-02-23 18:12:21 +0100

    created loader function in order page, about to start building order ui

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { getOrder } from "../services/apiRestaurant";
+import { useLoaderData } from "react-router-dom";
 
 const Order = () => {
+  const order = useLoaderData();
+  const {id} = order;
   return (
-    <div>Order</div>
-  )
-}
+    <div className="w-[95%] sm:w-[90%] lg:w-[50%] mx-auto my-5 sm:my-10">
+      <div>
+        <h2>
+          Order <span>#{}</span>
+        </h2>
+      </div>
+    </div>
+  );
+};
 
-export default Order
+export default Order;
+
+export const loader = async ({ params }) => {
+  const order = await getOrder(params.orderId);
+  console.log(order);
+
+  return order;
+};
+
+// http://localhost:5173/order/BQZXVP
