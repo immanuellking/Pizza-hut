cdc7f03194bd89edd110a8e86ab786108ae1d589
immanuellking <emmaebubeegbosi@gmail.com>
2024-02-24 08:42:33 +0100

    implement export function for formatting the date

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -4,5 +4,16 @@ export function formatCurrency(value) {
     currency: "EUR",
   });
 
-  return formatter.format(value)
+  return formatter.format(value);
+}
+
+export function formatDate(dateStr) {
+  const formatter = new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "short",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
+  return formatter.format(new Date(dateStr));
 }
