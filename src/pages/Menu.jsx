d6d93397a83201220e8d69268975af8a9d829ce1
immanuellking <emmaebubeegbosi@gmail.com>
2024-02-24 11:47:05 +0100

    made changes to the layout

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -8,7 +8,7 @@ const Menu = () => {
   const menu = useLoaderData();
   const cart = useSelector((state) => state.cart);
 
-  return <ul className="w-[90%] mx-auto my-16 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-x-5 gap-y-">
+  return <ul className="w-[90%] mx-auto my-16 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-8 xl:gap-x-12 gap-y-6 xl:gap-y-8">
     {menu.map((pizza) => (
         <MenuItem pizza={pizza} key={pizza.id} />
     ))}
