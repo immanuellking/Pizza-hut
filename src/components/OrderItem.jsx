e3497e86b4baaa618ba48d3511feb10ba99a96c4
immanuellking <emmaebubeegbosi@gmail.com>
2024-02-24 11:01:40 +0100

    order item ui implemented

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { formatCurrency } from "../utils/formatters";
 
-const OrderItem = () => {
+const OrderItem = ({ item, ingredients, isLoadingIngredients }) => {
+  const { quantity, name, totalPrice } = item;
   return (
-    <div>OrderItem</div>
-  )
-}
+    <li className="w-full flex justify-between items-center border-b-[1px] border-gray-200 py-4 gap-x-4">
+      <div className="">
+        <p>
+          <span className="font-bold">{quantity}&times;</span> {name}
+        </p>
+        <p className="italic text-gray-500">
+          {isLoadingIngredients ? "Loading...." : ingredients.join(", ")}
+        </p>
+      </div>
+      <div className="flex items-center gap-x-1">
+        <p className="font-bold text-lg">{formatCurrency(totalPrice)}</p>
+        <p className="text-gray-600">(NGN{(totalPrice * 1700).toLocaleString()})</p>
+      </div>
+    </li>
+  );
+};
 
-export default OrderItem
+export default OrderItem;
