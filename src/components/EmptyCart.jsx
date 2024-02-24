d5993c083508b6eaaa3628ccce4dc6180e39c96b
immanuellking <emmaebubeegbosi@gmail.com>
2024-02-24 13:39:01 +0100

    empty cart ui component imlemented

---
+++ src/components/EmptyCart.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const EmptyCart = () => {
+  return (
+    <div className="w-full flex flex-col items-center justify-center space-y-4">
+      <img src="/empty.avif" className="w-64" alt="empty" />
+      <div>
+        <h1 className="font-bold text-3xl">Your cart is empty</h1>
+      </div>
+    </div>
+  );
+};
+
+export default EmptyCart;
