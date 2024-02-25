d3de5bdd9fc4ef63b7eab35e7316b75ee4806e29
immanuellking <emmaebubeegbosi@gmail.com>
2024-02-25 11:36:54 +0100

    reduced space in buttons nd made the height of empty cart page full

--- src/components/EmptyCart.jsx
+++ src/components/EmptyCart.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const EmptyCart = () => {
   return (
-    <div className="w-full h-dvh flex flex-col items-center justify-center space-y-4">
+    <div className="w-full h-full flex flex-col items-center justify-center space-y-4">
       <img src="/empty.avif" className="w-64" alt="empty" />
       <div>
         <h1 className="font-bold text-3xl">Your cart is empty</h1>
