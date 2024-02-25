7f62714d5f6bf5ffb95510f727dd23652c4b97b6
immanuellking <emmaebubeegbosi@gmail.com>
2024-02-25 09:45:00 +0100

    added a new style property for the modal button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,7 +7,8 @@ const Button = ({ children, type, onClick, to, disabled }) => {
   const styles = {
     round: `${base}bg-gray-400 text-stone-800 hover:bg-gray-300 focus:outline-none focus:ring focus:ring-gray-400 focus:ring-offset-2 disabled:cursor-not-allowed p-1.5 sm:px-2 sm:py-2`,
     primary: `${base} bg-black text-white p-2 sm:px-12 sm:py-2.5`,
-    secondary: `${base} bg-black text-white px-20 py-4 text-lg`,
+    modal: `${base} bg-black text-white px-8 py-3 sm:px-12 sm:py-2.5`,
+    secondary: `${base} bg-black text-white px-20 py-5 text-lg`,
     disabled: `rounded-full font-semibold capitalize tracking-wide transition-colors duration-300 bg-[rgba(0,0,0,0.7)] text-white px-20 py-4 text-lg cursor-not-allowed`,
   };
 
